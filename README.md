12040c0ad29f067d0f7e4cf7803ee5aa4336b0ad
Franklin Carvajal <Franklin.a.carvajal@gmail.com>
2019-07-21 22:47:55 -0700

    Add Filtering, Pagination, & Sorting to gql. Add readme.

---
+++ README.md
@@ -0,0 +1,16 @@
+# Hackernews-node
+
+### Built a GraphQL server from scratch.
+
+The stack used was based on **Node.js, graphql-yoga and Prisma.**
+
+`graphql-yoga` is a fast and simple GraphQL server library `built on top of Express.js`. It comes with several features, such as out-of-the-box support for `GraphQL Playgrounds and realtime GraphQL subscriptions.`
+
+The resolvers of my GraphQL server are implemented using the `Prisma client that’s responsible for database access.`
+
+Deeper dive /  GraphQL community resources:
+
+    Prisma Blog: The blog regularly features new and interesting content about GraphQL, from community news to technical deep dives and various tutorials.
+    GraphQL Weekly: A weekly GraphQL newsletter with news from the GraphQL ecosystem
+    GraphQL Conf: The world’s biggest gathering of GraphQL enthusiasts happening in the heart of Berlin
+    Prisma Slack: A Slack team with vivid discussions around everything GraphQL & Prisma
--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,7 +1,40 @@
-function feed(parent, args, context, info) {
-    return context.prisma.links()
-}
+async function feed(parent, args, context, info) {
+    const where = args.filter ? {
+      OR: [
+        { description_contains: args.filter },
+        { url_contains: args.filter },
+      ],
+    } : {}
+
+    const links = await context.prisma.links({
+      where,
+      skip: args.skip,
+      first: args.first,
+      orderBy: args.orderBy
+    })
+
+    const count = await context.prisma
+    .linksConnection({
+      where,
+    })
+    .aggregate()
+    .count()
+
+    return {
+        links,
+        count
+    }
+  }
+
 
 module.exports = {
     feed,
 }
+
+/**
+ * If no filter string is provided, then the where object will be just an empty object and no filtering conditions will be applied by the Prisma engine when it returns the response for the links query.
+ * In case there is a filter carried by the incoming args, we’re constructing a where object that expresses my two filter conditions from above. This where argument is used by Prisma to filter out those Link elements that don’t adhere to the specified conditions.
+ * using the provided filtering, ordering and pagination arguments to retrieve a number of Link elements.
+ * using the linksConnection query from the Prisma client API to retrieve the total number of Link elements currently stored in the database.
+ * The links and count are then wrapped in an object to adhere to the Feed type added to the GraphQL schema.
+ */
